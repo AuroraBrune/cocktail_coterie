3450d94697d8ed82f929d7efbe77f9af8c2d842c
AnthonyStematz-Breitling <stembreit@gmail.com>
2020-09-16 10:44:31 -0500

    added a js directory to public, added a petite.js file, commented some psuedo code, connected petite.js to main.handlebars

---
+++ public/js/petite.js
@@ -0,0 +1,3 @@
+//exterior nav bar includes : about, contact, login / sign up
+//interior nav bar includes : personal info, log-out, 
+//search for cocktails, saved cocktails, create a new Soiree, Soiree history
