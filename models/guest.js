cd96b8951a3bfcaa6bcd495c340ee59c4396be29
Julie Schaub <julie.schaub@hotmail.com>
2020-09-18 13:14:43 -0500

    added drink table

--- models/guest.js
+++ models/guest.js
@@ -1,55 +1,55 @@
 module.exports = function(sequelize, DataTypes) {
-    var Guest = sequelize.define("Guest", {
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
+  var Guest = sequelize.define("Guest", {
+      firstName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+        lastName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
               len: [1]
             }
-          },
-          lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-              }
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-              isEmail: true
-            }
-          },
-          prefLiquor: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-              }
-          },
-          prefWine: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-              }
-          },
-          prefBeer: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-              }
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true
           }
+        },
+        prefLiquor: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1]
+            }
+        },
+        prefWine: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1]
+            }
+        },
+        prefBeer: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1]
+            }
+        }
+  });
+
+  Guest.associate = function(models) {
+    Guest.hasMany(models.User, {
+      onDelete: "cascade"
     });
-  
-    Guest.associate = function(models) {
-      Guest.hasMany(models.User, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return Guest;
-  }; 
+  };
+
+  return Guest;
+}; 
