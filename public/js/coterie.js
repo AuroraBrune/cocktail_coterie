3d737e0b169b62126581168562af0cbc7f3cf943
AnthonyStembreit <stembreit@gmail.com>
2020-09-19 12:58:52 -0500

    added id's to all buttons, psuedo-coded js file set up

--- public/js/coterie.js
+++ public/js/coterie.js
@@ -1,8 +1,19 @@
 //exterior nav bar includes : about, contact, & login / sign up
 //interior nav bar includes : 
-//personal info has edite button
-//log-out button
-//search for cocktails, search button, save cocktial button
-// saved cocktails, delete cocktail
-// new party, create invitation button, select cocktail
-//
+//log-out button --authentication.js??
+
+//personal info] edit button and then a save button -personal.js
+//edit button will show form to edit personal info
+//save button will update that information in the database
+
+//search for cocktails] search button, save cocktial button  -search.js
+//search button will call the cocktail api and write response on the page
+//save cocktial will update the database
+
+// saved cocktails] delete cocktail  -saved-cocktail.js
+//delete button will delete from the database
+
+// new party] create invitation button, select cocktail -new-party.js
+//select cocktial will find the selected cocktial in the database and feed the information into the new html 
+//create invitation button will take the inputs from the user and write a new html file user_id + ".html"
+
