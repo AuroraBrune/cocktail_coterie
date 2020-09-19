d8dd5e046e83faac479a041bce078748c9b86970
Julie Schaub <julie.schaub@hotmail.com>
2020-09-19 10:32:55 -0500

    added saved cocktails db

---
+++ models/saved-cocktail.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+    var SavedCocktail = sequelize.define("SavedCocktail", {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+          },
+          cocktailID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+              }
+          }
+    });
+  
+    SavedCocktail.associate = function(models) {
+      SavedCocktail.hasMany(models.User, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return SavedCocktail;
+  }; 
