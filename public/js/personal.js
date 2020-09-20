a90e66147e0a300b76aeb90c1c05d5d6fc4729d5
AnthonyStembreit <stembreit@gmail.com>
2020-09-20 15:19:58 -0500

    adding onlclick skeletons

--- public/js/personal.js
+++ public/js/personal.js
@@ -1,5 +1,11 @@
 //personal info] edit button and then a save button -personal.js
 
 //edit button will show form to edit personal info
+$("edit-personal").on("click", function(){
+    
+})
 
 //save button will update that information in the database
+$("save-personal").on("click", function(){
+
+})
--- public/js/saved-cocktails.js
+++ public/js/saved-cocktails.js
@@ -1,3 +1,14 @@
 // saved cocktails] delete cocktail  -saved-cocktail.js
+function renderCocktials(){
+    $.get
+}
 
+
+//onclicks
 //delete button will delete from the database
+
+$("#delete-cocktail").on("click", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+})
