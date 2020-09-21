e0ac8e9e814c4d9c807046e5b8d238060fbab3a1
Charlene Peters <cpeters1402@gmail.com>
2020-09-21 18:02:32 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,5 @@
+//This information probably needs to be transferred to the API routes
+
 const express = require('express');
 const router = express.Router();
 const authController = require('../config/auth');
